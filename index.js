4fc40778691fc48dc8760d9aad7f81ca07e42b56
Ebram atalla <ebraamatalla277@gmail.com>
2025-09-13 00:13:36 +0300

    Fix Index

--- index.js
+++ index.js
@@ -6,9 +6,5 @@ app.get('/bye', (req, res) => res.send('bye bye'));
 
 const port = process.env.PORT || 3000;
 
-// شغّل السيرفر فقط لما الملف يتنفذ مباشرة (مش من Jest)
-if (require.main === module) {
-  app.listen(port, () => console.log(`Server on :${port}`));
-}
+app.listen(port, () => console.log(`Server on :${port}`));
 
-module.exports = app; // للتست
