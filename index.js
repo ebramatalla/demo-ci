31cbd52a425bb65885325738bfd76b41b5ee40c2
Ebram atalla <ebraamatalla277@gmail.com>
2025-09-13 02:21:53 +0300

    test2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/hello', (req, res) => res.send('hello world'));
 app.get('/bye', (req, res) => res.send('bye bye'));
 app.get('/', (req, res) => res.send('Hello World222222222222'));
 app.get('/test', (req, res) => res.send('test Ci/Cd'));
+app.get('/test2', (req, res) => res.send('test Ci/Cd2'));
 const port = process.env.PORT || 3000;
 if (require.main === module) {
   app.listen(port, () => console.log(`Server on :${port}`));
