b8bef9f8eaa97f71bb7c5a4ad251cc9289f19442
Ebram atalla <ebraamatalla277@gmail.com>
2025-09-13 01:26:21 +0300

    Fix Laaaaaaaaaaast

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
-
 app.get('/hello', (req, res) => res.send('hello world'));
 app.get('/bye', (req, res) => res.send('bye bye'));
-
 const port = process.env.PORT || 3000;
-
 if (require.main === module) {
   app.listen(port, () => console.log(`Server on :${port}`));
 }
-
-module.exports = app;   
+module.exports = app;
