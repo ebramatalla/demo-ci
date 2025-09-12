c89d92d68f0d84684eff1b2a34112cc14d91c094
Ebram atalla <ebraamatalla277@gmail.com>
2025-09-12 17:33:20 +0300

    bye bye

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => res.send('hello world'));
+app.get('/bye', (req, res) => res.send('bye bye'));
 
 const port = process.env.PORT || 3000;
 
