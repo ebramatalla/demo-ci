332b868eb7fe5128b68c6c54ef7ec2b81ad5de9a
Ebram atalla <ebraamatalla277@gmail.com>
2025-09-13 01:54:32 +0300

    remove bye

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => res.send('hello world'));
-app.get('/bye', (req, res) => res.send('bye bye'));
 
 const port = process.env.PORT || 3000;
 if (require.main === module) {
