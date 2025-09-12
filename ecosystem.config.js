cafa183d45ed7daac0b1ec4a8b1ea14970e6b8ba
Ebram atalla <ebraamatalla277@gmail.com>
2025-09-13 01:19:38 +0300

    Fix Eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,16 @@ module.exports = {
     name: "demo-ci",
     script: "index.js",
     instances: 1,
-    env: { PORT: 3000, NODE_ENV: "development" },
-    env_production: { PORT: 3000, NODE_ENV: "production" }
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    env: {
+      PORT: 3000,
+      NODE_ENV: "development"
+    },
+    env_production: {
+      PORT: 3000,
+      NODE_ENV: "production"
+    }
   }]
 }
