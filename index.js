1e085f7d471102a7ec62fe39654d90b119d2f4a0
Ebram atalla <ebraamatalla277@gmail.com>
2025-09-13 01:10:38 +0300

    Fix

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ app.get('/bye', (req, res) => res.send('bye bye'));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server on :${port}`));
+if (require.main === module) {
+  app.listen(port, () => console.log(`Server on :${port}`));
+}
 
+module.exports = app;   // متصدّرش server، بس app
