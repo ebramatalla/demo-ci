0c82abc91120a0a8b851017346e32694042e510f
Ebram atalla <ebraamatalla277@gmail.com>
2025-09-13 02:01:12 +0300

    test ci cd

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 app.get('/hello', (req, res) => res.send('hello world'));
 app.get('/bye', (req, res) => res.send('bye bye'));
 app.get('/', (req, res) => res.send('Hello World222222222222'));
+app.get('/test', (req, res) => res.send('test Ci/Cd'));
 const port = process.env.PORT || 3000;
 if (require.main === module) {
   app.listen(port, () => console.log(`Server on :${port}`));
